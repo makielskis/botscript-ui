makielskis/botscript-ui

commit c8b9eea18aedb6e6a0efdc327652629df7e0a0fc

fix #15: show newly created bot after creation

js/bot_ui.js
@@ -1209,9 +1209,8 @@ $(function() {
       });
 
       // show first panel
-      $("#bot-switcher-dd a").first().click();
+      $("#bot-switcher-dd a").get(-2).click();
       $("#bot-switcher").first().click();
-
 
       if (!BotSwitcher.initialized) {
         // Phone menu and logout

commit 85adbae27ee3f7311c20f0f7472c04d8c4911349

fix error when no bot exists introduced with c8b9eea

js/bot_ui.js
@@ -1208,8 +1208,12 @@ $(function() {
         });
       });
 
-      // show first panel
-      $("#bot-switcher-dd a").get(-2).click();
+      // show last bot panel or create bot form
+      if ($("#bot-switcher-dd a").length < 2) {
+        $("#bot-switcher-dd a").first().click();
+      } else {
+        $("#bot-switcher-dd a").get(-2).click();
+      }
       $("#bot-switcher").first().click();
 
       if (!BotSwitcher.initialized) {