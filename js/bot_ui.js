58f23023e5ae938a05d3c4e1a46ba2424b5125f2
Felix Gündling <felixguendling@gmail.com>
2014-04-19 16:36:32 +0200

    no update if no widget

--- js/bot_ui.js
+++ js/bot_ui.js
@@ -1369,7 +1369,9 @@ $(function() {
         }
 
         // update coresponding widget if existing (might not exist if bot is newly created)
-        if (_.isObject(this.widgets[botid])) {
+        if (_.isObject(this.widgets[botid])
+            && _.isObject(this.widgets[botid][module])
+            && _.isObject(this.widgets[botid][module][property])) {
           var isFromProperty = false;
           if (property.slice(-5) === "_from") {
             property = property.slice(0, -5);
